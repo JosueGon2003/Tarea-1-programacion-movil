c42eab57f680fbe1f642ff56e2dc2ecc73a98a4f
JosueGon2003 <wortega252@gmail.com>
2024-10-25 18:38:37 -0500

    login aumentado

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
       navbarMenu.classList.toggle('hidden');
     });
   });
+
+  function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    if (username === 'admin' && password === 'password123') {
+      alert('Inicio de sesión exitoso');
+      window.location.href = 'index.html'; 
+    } else {
+      alert('Usuario o contraseña incorrectos');
+    }
+  }
+
+
+  
